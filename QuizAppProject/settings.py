15c3c86ad9162e754ab1032dae7b64f758dad965
Pyhika <skylightblue430@gmail.com>
2021-02-17 15:57:06 +0900

    edit: HOSTの変更

--- QuizAppProject/settings.py
+++ QuizAppProject/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = '7*b73)j+4@41zrn2_6g#y!7x0#yu+fp5e$c=m-dc6esk0$yn01'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['diary-quiz-app.herokuapp.com']
+ALLOWED_HOSTS = ['diary-quiz-app.herokuapp.com/login']
 
 
 # Application definition
