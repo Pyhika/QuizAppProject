7a69e9ecb8b84f8eebea6e45157ee62a6c0a0730
Pyhika <skylightblue430@gmail.com>
2021-02-21 01:26:24 +0900

    edit: servererror:500の対処

--- QuizAppProject/settings.py
+++ QuizAppProject/settings.py
@@ -1,6 +1,7 @@
 # 本番環境
 from .settings_common import *
 import os
+import dj_database_url
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
@@ -8,3 +9,5 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['diary-quiz-app.herokuapp.com']
 
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
