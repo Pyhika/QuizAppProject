47669919f36fc31b8ebe25d2c3bdf7460dc2cbf8
Pyhika <skylightblue430@gmail.com>
2021-02-20 23:06:03 +0900

    edit: servererror:500解消

--- QuizAppProject/settings.py
+++ QuizAppProject/settings.py
@@ -2,8 +2,7 @@
 from .settings_common import *
 import os
 
-SECRET_KEY = '7*b73)j+4@41zrn2_6g#y!7x0#yu+fp5e$c=m-dc6esk0$yn01'
-# SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 DEBUG = False
 
